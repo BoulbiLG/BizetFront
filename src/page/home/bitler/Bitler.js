d003f17b47dc7b571195af838e36735a64fc84e0
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-11-17 00:11:24 +0100

    m

--- src/page/home/bitler/Bitler.js
+++ src/page/home/bitler/Bitler.js
@@ -7,6 +7,7 @@ const Bitler = () => {
     <div className='bitlerConteneur'>
         <div className="autreConteneur">
             <p className='description'>Ce site internet archive également le travail du forumeur du forum blabla 18 25 de JeuxVideo.com et du forum Onche.org</p>
+            <p>Rien pour le moment</p>
         </div>
     </div>
   )
