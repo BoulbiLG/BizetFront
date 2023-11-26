a5d89adbe8ec5311fe694f45b2c19f74dfefc6c6
pierrebenard <118182025+pierrebenard@users.noreply.github.com>
2023-11-26 18:12:47 +0100

    m

--- src/page/home/HomeTerminal.js
+++ src/page/home/HomeTerminal.js
@@ -50,31 +50,33 @@ const HomeTerminal = () => {
               </div>
             ) : null }
           </div>
-          <div className="haut2" style={styleHaut2}>
-            <Bouton className='alaune ongletFenetre' label='A la une' onClick={() => {fenetreSet('A la une'); globalFenetreAlaUneSet('liste')}} />
-            {/*<Bouton className='decouvrir ongletFenetre' label='Découvrir' onClick={() => {fenetreSet('Découvrir')}} />*/}
-            <Bouton className='rechercher ongletFenetre' label='Rechercher' onClick={() => {fenetreSet('rechercher'); globalFenetreAlaUneSet('liste')}} />
-            {/*<Bouton className='compositeur ongletFenetre' label='Compositeur' onClick={() => {fenetreSet('Compositeur')}} />*/}
-            <Bouton className='mecontacter ongletFenetre' label='Me contacter' onClick={() => {fenetreSet('contact'); globalFenetreAlaUneSet('liste')}} />
-            {/*<Bouton className='discussion ongletFenetre' label='Discussion' onClick={() => {fenetreSet('Discussion')}} />*/}
-            <Bouton className='bitler ongletFenetre' label='Archive de Bitler' onClick={() => {fenetreSet('bitler'); globalFenetreAlaUneSet('liste')}} />
-            { verificationAuth === 'true' ? (
-              <div className="bas">
-                <Bouton className='monprofil ongletFenetre' label='Mon profil' onClick={() => {fenetreSet('profil'); globalFenetreAlaUneSet('liste')}} />
+          <div className="centralisationHaut2">
+            <div className="haut2" style={styleHaut2}>
+              <Bouton className='alaune ongletFenetre' label='A la une' onClick={() => {fenetreSet('A la une'); globalFenetreAlaUneSet('liste')}} />
+              {/*<Bouton className='decouvrir ongletFenetre' label='Découvrir' onClick={() => {fenetreSet('Découvrir')}} />*/}
+              <Bouton className='rechercher ongletFenetre' label='Rechercher' onClick={() => {fenetreSet('rechercher'); globalFenetreAlaUneSet('liste')}} />
+              {/*<Bouton className='compositeur ongletFenetre' label='Compositeur' onClick={() => {fenetreSet('Compositeur')}} />*/}
+              <Bouton className='mecontacter ongletFenetre' label='Me contacter' onClick={() => {fenetreSet('contact'); globalFenetreAlaUneSet('liste')}} />
+              {/*<Bouton className='discussion ongletFenetre' label='Discussion' onClick={() => {fenetreSet('Discussion')}} />*/}
+              <Bouton className='bitler ongletFenetre' label='Archive de Bitler' onClick={() => {fenetreSet('bitler'); globalFenetreAlaUneSet('liste')}} />
+              { verificationAuth === 'true' ? (
+                <div className="bas">
+                  <Bouton className='monprofil ongletFenetre' label='Mon profil' onClick={() => {fenetreSet('profil'); globalFenetreAlaUneSet('liste')}} />
+                </div>
+              ) : null }
+              <div className="fermerSection">
+                <Bouton className='fermer ongletFenetre' label='Fermer' onClick={() => {
+                  styleHaut2Set({animation: 'disparition 0.2s ease-in-out forwards'});
+                  styleMenuBtnSet({animation: 'arrive 0.2s ease-in-out forwards'});
+                }}/>
               </div>
-            ) : null }
-            <div className="fermerSection">
-              <Bouton className='fermer ongletFenetre' label='Fermer' onClick={() => {
-                styleHaut2Set({animation: 'disparition 0.2s ease-in-out forwards'});
-                styleMenuBtnSet({animation: 'arrive 0.2s ease-in-out forwards'});
-              }}/>
             </div>
-          </div>
-          <div className="menuSectionBtn" style={styleMenuBtn}>
-            <button className='menuOnglet ongletFenetre' label='Menu' onClick={() => {
-              styleHaut2Set({animation: 'arrive 0.2s ease-in-out forwards'});
-              styleMenuBtnSet({animation: 'disparition 0.2s ease-in-out forwards'});
-            }}>Menu</button>
+            <div className="menuSectionBtn" style={styleMenuBtn}>
+              <button className='menuOnglet ongletFenetre' label='Menu' onClick={() => {
+                styleHaut2Set({animation: 'arrive 0.2s ease-in-out forwards'});
+                styleMenuBtnSet({animation: 'disparition 0.2s ease-in-out forwards'});
+              }}>Menu</button>
+            </div>
           </div>
         </div>
         <div className="sectionFenetre">
